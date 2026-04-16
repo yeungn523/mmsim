9985b5e26aef156ba43915f863318022eb2f9998
Natalie Yeung <127846410+yeungn523@users.noreply.github.com>
2026-04-16 10:29:10 -0400

    Added test automation setup script

---
+++ setup.ps1
@@ -0,0 +1,4 @@
+# C:\Users\nyy4\Downloads\mmsim\setup.ps1
+$env:PATH += ";C:\programs\intelFPGA\18.1\modelsim_ase\win32aloem"
+$env:PYTHONPATH = "C:\Users\nyy4\Downloads\mmsim\src"
+Write-Host "Environment ready"
